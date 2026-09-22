6c99990bf9110c9fbedc72ef1a6fbe9f702c6381
rahulsd380 <rahulsd380@gmail.com>
2026-09-22 11:02:58 +0600

    Fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -559,7 +559,7 @@ const Home: React.FC = () => {
       </section>
 
       {/* --- News Section --- */}
-      <section className="py-24 bg-[#f8f6f1]">
+      {/* <section className="py-24 bg-[#f8f6f1]">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
             <h2 className="text-3xl md:text-4xl font-bold text-[#0d0d0d] mb-4">
@@ -614,7 +614,7 @@ const Home: React.FC = () => {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* --- CTA Section --- */}
       <section className="py-24 bg-white">
